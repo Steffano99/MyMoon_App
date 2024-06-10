2962850a417996d9a90bea765f03d9d512b18e61
Steffano99 <101764484+Steffano99@users.noreply.github.com>
2024-06-10 00:38:20 +0000

    first commit

---
+++ my_moon_app/lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage("assets/images/moon.jpg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
