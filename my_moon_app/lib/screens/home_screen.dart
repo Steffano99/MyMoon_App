09adab7e4a3ecc3b9f2c7b0760de140846e867d6
Steffano99 <101764484+Steffano99@users.noreply.github.com>
2024-10-03 16:16:28 +0000

    additional changes

--- my_moon_app/lib/screens/home_screen.dart
+++ my_moon_app/lib/screens/home_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:ffi';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_moon_app/widgets/custom_dropdown.dart';
 
--- my_moon_app/lib/widgets/custom_dropdown.dart
+++ my_moon_app/lib/widgets/custom_dropdown.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class CustomDropdownButton extends StatelessWidget {
   late List<String> values;
   late double width;
